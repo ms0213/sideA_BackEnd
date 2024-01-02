28797912ed26d2cbeba3902352d86d994ecb0897
kimminsun <kimminsun@218.38.137.28>
2024-01-02 19:48:34 +0900

    git test

---
+++ sideA-BackEnd/src/main/java/com/example/sideA/SideABackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.sideA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideABackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideABackEndApplication.class, args);
+	}
+
+}
---
+++ sideA-BackEnd/src/test/java/com/example/sideA/SideABackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sideA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideABackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
